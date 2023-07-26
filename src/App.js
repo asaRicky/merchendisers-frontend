b743fc6632deb70718b1cfbb7536570162e6618c
Derrick <derrickomondi208@gmail.com>
2023-07-26 12:11:51 +0300

    completed Navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import { BrowserRouter as Router, Link, Route} from "react-router-dom";
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import Dropdown from './Dropdown';
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -53,15 +56,7 @@ function Navbar() {
             </Link>
             {dropdown && <Dropdown />}
           </li>
-          <li className='nav-item'>
-            <Link
-              to='/products'
-              className='nav-links'
-              onClick={closeMenu}
-            >
-              Products
-            </Link>
-          </li>
+          
           <li className='nav-item'>
             <Link
               to='/contact-us'
