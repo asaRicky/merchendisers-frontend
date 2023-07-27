49aca1ef8bd223204d739c569d663ab8bbf2f2fa
Derrick <derrickomondi208@gmail.com>
2023-07-27 10:50:21 +0300

    completed Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import { BrowserRouter as Router, Link, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+// import Home from './components/Home';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Link>
-         <Route path='/' exact component={Home} />
-      </Link>
+      {/* <Route path="/" exact component={Home} /> */}
     </Router>
   );
 }
