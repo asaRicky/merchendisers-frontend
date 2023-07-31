c176810d216f8c6742e17d4dd8b85bc100d4c112
Ruth <rwangare124@gmail.com>
2023-07-31 22:50:13 +0300

    working on the homepage

---
+++ src/components/pages/LandingPag.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const LandingPage = () => {
+    return (
+      <div>
+        <header>
+          <div className="container">
+            <div className="row">
+              <a className="logo" href="/" title="GPS Tracking Software" alt="GPS Tracking Software">
+                <picture>
+                  <source loading="lazy" srcSet="/assets/images/logo.webp" type="image/webp" width="172" height="50" />
+                  <source loading="lazy" srcSet="/assets/images/logo.png" type="image/png" width="172" height="50" />
+                  <img loading="lazy" src="/assets/images/logo.png" alt="MerchTracker" width="172" height="50" />
+                </picture>
+              </a>
+              <nav>
+              <div id="navtab" className="mobnav"></div>
+              <ul id="mobsnow">
+                <li>
+                  <a href="/" title="Home">Home</a>
+                </li>
+                {/* Rest of the menu items */}
+              </ul>
+            </nav>
+          </div>
+        </div> </header>
+      <section className="banner-area">
+        {/* Rest of the banner area content */}
+      </section>
+      <section className="videoRow">
+        {/* Rest of the videoRow content */}
+      </section>
+      <section className="tracking-solution">
+        {/* Rest of the tracking-solution content */}
+      </section>
+    </div>
+  );
+};
+
+              export default LandingPage;
