39d7fbe97bb37c474fa59b6cc1fd2e41f90f7b94
nicole <nicolenamayan133@gmail.com>
2023-08-01 14:31:17 +0300

    working on homepage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Footer from './components/Footer';
+import Home from './components/pages/Home';
 import Mechants from './components/pages/Mechants';
 import Manager from './components/pages/Manager';
 import SignUp from './components/Signup';
@@ -13,13 +13,11 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />}/>
         <Route path="/manager" element={<Manager />}/>
         <Route path="/merchants" element={<Mechants/>}/>
         <Route path="/signup" element={<SignUp/>}/>
-
       </Routes>
-
-      <Footer />
     </Router>
   );
 }
